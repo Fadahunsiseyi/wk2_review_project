4ddb959d6e76059231579d9eb8e6929d9f67b48c
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2023-02-09 10:57:50 +0100

    generate product alerts component

---
+++ Downloads/angular-trwhjk/README.md
@@ -0,0 +1,3 @@
+# angular-trwhjk
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-trwhjk)
---
+++ Downloads/angular-trwhjk/src/app/product-alerts/product-alerts.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-alerts',
+  templateUrl: './product-alerts.component.html',
+  styleUrls: ['./product-alerts.component.css']
+})
+export class ProductAlertsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
